1e5f98ac03125e4d30a3f8029de4c1ae044aff46
Lajos Seyler <lajosseyler@protonmail.com>
2024-11-15 18:28:48 +0100

    feat: add QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Navigate to="/login" />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />{" "}
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate to="/login" />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />{" "}
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
