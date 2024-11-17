7637b9fbc7804b0752edd71e6f99fa66c49d9a74
Lajos Seyler <lajosseyler@protonmail.com>
2024-11-17 21:40:26 +0100

    feat: add user to AuthStore

--- src/features/users/hooks/useAuthStore.tsx
+++ src/features/users/hooks/useAuthStore.tsx
@@ -1,12 +1,18 @@
 import { create } from "zustand";
 
 import AuthState from "../interfaces/AuthState";
+import User from "../interfaces/User";
 
 const useAuthStore = create<AuthState>((set) => ({
   token: "",
+  user: undefined,
   setToken: (accessToken: string) =>
     set({
       token: accessToken,
+    }),
+  setUser: (user: User) =>
+    set({
+      user,
     }),
   logout: async () => set({ token: "" }),
 }));
--- src/features/users/interfaces/AuthState.tsx
+++ src/features/users/interfaces/AuthState.tsx
@@ -1,5 +1,9 @@
+import User from "./User";
+
 export default interface AuthState {
   token: string;
+  user?: User;
   setToken: (accessToken: string) => void;
+  setUser: (user: User) => void;
   logout: () => Promise<void>;
 }
