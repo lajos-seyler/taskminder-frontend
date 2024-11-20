6795d40653ca78cee3eadeb505d49463723b8146
Lajos Seyler <lajosseyler@protonmail.com>
2024-11-20 12:07:33 +0100

    chore: create PageHeader component

---
+++ src/ui/PageHeader.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren } from "react";
+import styled from "styled-components";
+
+const StyledPageHeader = styled.h1`
+  background-color: white;
+  width: 100%;
+  padding: 0.75rem 1.5rem;
+  border-radius: 1rem;
+`;
+
+function PageHeader({ children }: PropsWithChildren) {
+  return <StyledPageHeader>{children}</StyledPageHeader>;
+}
+
+export default PageHeader;
